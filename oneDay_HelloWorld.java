d4aff6bbff3128711f2391a2b7e9a816cbf43605
lengs <lengsworkspace@gmail.com>
2023-09-09 17:34:20 +0800

    oneDay:注释补充

--- oneDay_HelloWorld.java
+++ oneDay_HelloWorld.java
@@ -1,5 +1,14 @@
+//Java入门1Day：演示java的开发步骤
+//这是对代码的相关说明
+//1.public class oneDay_HelloWorld 表示oneDay_HelloWorld是一个类，是一个public共有属性的类
+//2.Hello{}表示一个类的开始和结束
+//3.public static void main(String[] args)表示一个主方法，即程序的入口
+//4.main(){}表示方法的开始和结束
+//5.System.out.println("Hello World!");表示输出"Hello World!"到屏幕
+//6.;表示语句结束
 public class oneDay_HelloWorld {
     public static void main(String[] args){
+        //编写一个main方法
         System.out.println("Hello World!");
     }
 }
