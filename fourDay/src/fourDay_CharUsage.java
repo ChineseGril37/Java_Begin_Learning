28e36ba1cd2a9cc9fd097e13fdd4dccc93316a3e
Lengs <lengsworkspace@gmail.com>
2023-09-13 17:13:46 +0800

    Day3学习内容：char字符类型用法

---
+++ fourDay/src/fourDay_CharUsage.java
@@ -0,0 +1,13 @@
+//演示char的用法
+public class fourDay_CharUsage {
+    public static void main(String[] args) {
+        char c1 = 'a';
+        char c2 = '\t';
+        char c3 = '冷';
+        char c4 = 97;//说明:字符类型可以直接存放一个数字
+        System.out.println(c1);
+        System.out.println(c2);
+        System.out.println(c3);
+        System.out.println(c4);//ascii码 97 = a
+    }
+}
