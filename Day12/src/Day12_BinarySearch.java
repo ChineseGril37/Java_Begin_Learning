de75dbc647e21b8a7334713036464c16619c0a78
Lengs <lengsworkspace@gmail.com>
2023-11-23 17:28:50 +0800

    Day12学习内容：数组查询，二分查找

---
+++ Day12/src/Day12_BinarySearch.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class BinarySearch {
+
+}
+public class Day12_BinarySearch {
+
+    public static void main(String[] args){
+        //2) 请对一个有序数组进行二分查找 {1,8, 10, 89, 1000, 1234} ，输入一个数看看该数组是否存在此数，并且求出下标，
+        //如果没有就提示"没有这个数
+        int[] arr = {1,8, 10, 89, 1000, 1234};
+        Scanner myScanner = new Scanner(System.in);
+        System.out.println("请输入要查找的数字:");
+        new BinarySearch();
+    }
+}
