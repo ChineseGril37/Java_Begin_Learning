42d77d95264213ea5cc771133fef56e0e45ca03d
lengs <lengsworkspace@gmail.com>
2024-03-02 12:50:39 +0800

    Day14学习内容：不同方法中互相调用的传参机制

--- Day15/src/main/java/org/lengs/Day15_MethodParameter02.java
+++ Day15/src/main/java/org/lengs/Day15_MethodParameter02.java
@@ -1,7 +1,27 @@
-package org.lengs;
-
 public class Day15_MethodParameter02 {
     public static void main(String[] args){
-
+        A a = new A();
+        a.age = 10;
+        B b = new B();
+        System.out.println("B类中置空前\na.age = " + a.age);
+        b.clear(a);
+        System.out.println("B类中置空后\na.age = " + a.age);
+        System.out.println("B类中重置前\na.age = " + a.age);
+        b.reset(a);
+        System.out.println("B类中重置后\na.age = " + a.age);
+    }
+}
+class A{
+    String name;
+    int age;
+}
+class B{
+    public void clear(A a){
+        a = null;
+    }
+    public void reset(A a){
+        a = new A();
+        a.age = 18;
+        a.name = "???";
     }
 }
